76b75767be2814580c797db01e69c0eb5b3b9019
yoannduc <dev.yoannduc@gmail.com>
2019-12-05 17:42:47 +0100

    Implement logrus hook

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/yoannduc/logrus-dailyrotate
 
 go 1.13
+
+require (
+	github.com/sirupsen/logrus v1.4.2
+	github.com/yoannduc/dailyrotate v0.0.0-20191204223408-f5724536acd1
+)
--- logrus-dailyrotate.go
+++ logrus-dailyrotate.go
@@ -1,1 +1,60 @@
-package logrus-dailyrotate
+package logrusdailyrotate
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/yoannduc/dailyrotate"
+)
+
+type Hook struct {
+	rotateWriter *dailyrotate.RotateWriter
+	formatter    logrus.Formatter
+	minLevel     logrus.Level
+}
+
+func New(
+	p string,
+	ma int,
+	f logrus.Formatter,
+	ml logrus.Level,
+) (*Hook, error) {
+	rw, err := dailyrotate.New(p, ma)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Hook{
+		rotateWriter: rw,
+		formatter:    f,
+		minLevel:     ml,
+	}, nil
+}
+
+func NewWithDefaults() (*Hook, error) {
+	rw, err := dailyrotate.NewWithDefaults()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Hook{
+		rotateWriter: rw,
+		formatter:    &logrus.TextFormatter{},
+		minLevel:     logrus.InfoLevel,
+	}, nil
+}
+
+func (h *Hook) Fire(e *logrus.Entry) error {
+	m, err := h.formatter.Format(e)
+	if err != nil {
+		return err
+	}
+
+	if _, err = h.rotateWriter.RotateWrite([]byte(m)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (hook *Hook) Levels() []logrus.Level {
+	return logrus.AllLevels[:hook.minLevel+1]
+}
